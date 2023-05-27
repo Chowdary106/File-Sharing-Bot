5dbe7adf64ab69b0591cb7e0cdea4f937b8e0cfb
Chowdary <131300937+Chowdary106@users.noreply.github.com>
2023-05-27 15:19:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -85,8 +85,7 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
-        print("error")
-        )
+       
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
@@ -95,7 +94,7 @@ async def start_command(client: Client, message: Message):
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-            reply_markup = reply_markup,
+            
             disable_web_page_preview = True,
             quote = True
         )
